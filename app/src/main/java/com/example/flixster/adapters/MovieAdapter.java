4998bc26bc54845f7b3a0f6b2bd29ed7917ece15
Lorenzo Miro San Diego <lsandiego2@horizon.csueastbay.edu>
2021-08-27 23:46:47 -0700

    Features for movie list viewing are implemented

---
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,105 @@
+package com.example.flixster.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster.R;
+import com.example.flixster.models.Movie;
+
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    Context context;
+
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+
+        this.context = context;
+
+        this.movies = movies;
+
+    }
+
+
+    //inflate XML layout and return holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        Log.d("MovieAdapter ","onCreateViewHolder");
+
+        View movie_view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+
+        return new ViewHolder(movie_view);
+    }
+
+    //populate data through viewHolder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Log.d("MovieAdapter","onBindViewHolder " + position);
+
+        //get movie at position passed
+
+        Movie movie = movies.get(position);
+
+        //bind movie data -> viewholder
+
+        holder.bind(movie);
+
+    }
+
+    //return total item count
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle;
+
+        TextView tvOverview;
+
+        ImageView ivPoster;
+
+        //create a constructor to represent the ViewHolder
+
+        public ViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+
+            //define location of textViews + imageViews
+
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+
+        }
+
+        public void bind(Movie movie) {
+
+            tvTitle.setText(movie.getTitle());
+
+            tvOverview.setText(movie.getOverview());
+
+            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,65 @@
+package com.example.flixster.models;
+
+//holds data for movie in the database
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+
+    String posterPath;
+
+    String title;
+
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException { //take in JSON objects + get important data
+
+        posterPath = jsonObject.getString("poster_path");
+
+        title = jsonObject.getString("title");
+
+        overview = jsonObject.getString("overview");
+
+    }
+
+    public static List<Movie> fromJSONArray(JSONArray movie_json_array) throws JSONException {
+
+        List<Movie> movies = new ArrayList<>();
+
+        for (int m = 0; m < movie_json_array.length(); m++) { //loop used to add movies
+
+            //add movie into movies array
+
+            movies.add(new Movie(movie_json_array.getJSONObject(m)));
+            
+
+        }
+
+        return movies;
+
+    }
+
+    public String getPosterPath() {
+
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+
+    }
+
+    public String getTitle() {
+
+        return title;
+
+    }
+
+    public String getOverview() {
+
+        return overview;
+
+    }
+
+}
