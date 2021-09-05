3b9ae4459e2280ee14dd06cd58d863d2817f7db1
Lorenzo Miro San Diego <lsandiego2@horizon.csueastbay.edu>
2021-09-04 22:22:17 -0700

    Movie title can be seen when movie item is clicked

--- app/src/main/java/com/example/flixster/DetailActivty.java
+++ app/src/main/java/com/example/flixster/DetailActivty.java
@@ -3,17 +3,39 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class DetailActivty extends AppCompatActivity {
+
+    //implement textviews for movie overview + title, and rating bar
+
+    TextView tvTitle;
+
+    TextView tvOverview;
+
+    RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        //create title/overview textviews, rating bar
+
+        tvTitle = findViewById(R.id.tvTitle);
+
+        tvOverview = findViewById(R.id.tvOverview);
+
+        ratingBar = findViewById(R.id.ratingBar);
+
         //get title
 
         String title = getIntent().getStringExtra("title");
+
+        //push title to text view
+
+        tvTitle.setText(title);
 
     }
 }
